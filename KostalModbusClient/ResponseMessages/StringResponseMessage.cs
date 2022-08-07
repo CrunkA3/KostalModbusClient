856599005106b00b1f14230f7bc06123ab7f624f
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-08-07 10:05:41 +0200

    fix StringResponseMessage value length

--- KostalModbusClient/ResponseMessages/StringResponseMessage.cs
+++ KostalModbusClient/ResponseMessages/StringResponseMessage.cs
@@ -2,6 +2,12 @@ public class StringResponseMessage : ResponseMessage
 {
     private byte _length;
     public StringResponseMessage(byte[] bytes, byte length) : base(bytes) { _length = length; }
-    public string Value => System.Text.Encoding.UTF8.GetString(Bytes[9..(_length * 2 + 9)].TakeWhile(m => m > 0).ToArray());
+    public string Value
+    {
+        get
+        {
+            return System.Text.Encoding.UTF8.GetString(Bytes[9..(_length + 9)].TakeWhile(m => m > 0).ToArray());
+        }
+    }
 
 }
