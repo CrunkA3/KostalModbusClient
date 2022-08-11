fe89a9253d8a332be3f896af0f061e9e9845ee70
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-08-11 11:39:53 +0200

    add showcase app

---
+++ KostalModbusClient.Showcase/Program.cs
@@ -0,0 +1,23 @@
+﻿using KostalModbusClient;
+
+Console.WriteLine("Initialize client");
+
+var client = new ModbusClient("192.168.1.120");
+
+var totalAcPowerResponse = await client.GetTotalAcPower();
+
+var totalAcActivePowerResponse = await client.GetTotalAcActivePower();
+var totalAcReactivePowerResponse = await client.GetTotalAcReactivePower();
+var totalAcApparentPowerResponse = await client.GetTotalAcApparentPower();
+
+Console.Write(totalAcPowerResponse.ActivePower);
+Console.Write(" = ");
+Console.WriteLine(totalAcActivePowerResponse.Value);
+
+Console.Write(totalAcPowerResponse.ReactivePower);
+Console.Write(" = ");
+Console.WriteLine(totalAcReactivePowerResponse.Value);
+
+Console.Write(totalAcPowerResponse.ApparentPower);
+Console.Write(" = ");
+Console.WriteLine(totalAcApparentPowerResponse.Value);
