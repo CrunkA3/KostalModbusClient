e02c3d7285b701c4030d0cd0a7b28a1c4fd16037
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-08-11 11:41:43 +0200

    update showcase app

--- KostalModbusClient.Showcase/Program.cs
+++ KostalModbusClient.Showcase/Program.cs
@@ -6,18 +6,6 @@ var client = new ModbusClient("192.168.1.120");
 
 var totalAcPowerResponse = await client.GetTotalAcPower();
 
-var totalAcActivePowerResponse = await client.GetTotalAcActivePower();
-var totalAcReactivePowerResponse = await client.GetTotalAcReactivePower();
-var totalAcApparentPowerResponse = await client.GetTotalAcApparentPower();
-
-Console.Write(totalAcPowerResponse.ActivePower);
-Console.Write(" = ");
-Console.WriteLine(totalAcActivePowerResponse.Value);
-
-Console.Write(totalAcPowerResponse.ReactivePower);
-Console.Write(" = ");
-Console.WriteLine(totalAcReactivePowerResponse.Value);
-
-Console.Write(totalAcPowerResponse.ApparentPower);
-Console.Write(" = ");
-Console.WriteLine(totalAcApparentPowerResponse.Value);
+Console.WriteLine(totalAcPowerResponse.ActivePower);
+Console.WriteLine(totalAcPowerResponse.ReactivePower);
+Console.WriteLine(totalAcPowerResponse.ApparentPower);
